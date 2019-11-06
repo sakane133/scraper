e6d81f135b971a67c4fadd305487761891bff3cc
github username <s.kane133@gmail.com>
2019-11-05 22:30:52 -0500

    dotenv

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'http://rubygems.org'
+
+gem 'byebug'
+gem 'httparty'
+gem 'nokogiri'
+gem 'selenium-webdriver', '~> 2.53', '>= 2.53.4'
+gem "wait", "~> 0.5.3"
+gem "dotenv"
---
+++ scraper.rb
@@ -0,0 +1,63 @@
+require "nokogiri"
+require "httparty"
+require "byebug"
+require "selenium-webdriver"
+require "wait"
+require 'dotenv/load'
+
+def scraper
+    url = 'https://www.dominionenergy.com/sign-in'
+    unparsed_page = HTTParty.get(url)
+    parsed_page = Nokogiri::HTML(unparsed_page)
+end
+
+driver = Selenium::WebDriver.for :chrome
+driver.navigate.to 'https://www.dominionenergy.com/sign-in'
+wait = Selenium::WebDriver::Wait.new(:timeout => 10)
+
+# click login button
+login = driver.find_element(class: 'top-hitter')
+login.click
+
+# fill in username & password then submit
+wait.until { driver.find_element(:name, 'USER') }
+username = driver.find_element(:name, 'USER')
+un = ENV["UNAME"]
+username.send_keys "#{un}"
+
+password = driver.find_element(:name, 'PASSWORD')
+pw = ENV["PW"]
+password.send_keys "#{pw}"
+
+submit = driver.find_element(:id, 'btnSubmit')
+submit.click
+
+ byebug
+
+# due date
+wait.until { driver.find_element(:class, 'bodyTextGreen') }
+due_date = driver.find_elements(:class, 'bodyTextGreen')[0].text
+
+# bill amount
+bill_amount = driver.find_elements(:class, 'bodyTextGreen')[1].text
+
+# get to usage page
+usage_page = driver.find_element(:link, 'Analyze Energy Usage')
+usage_page.click
+
+# start date
+start_date = driver.find_elements(:tag_name, "td")[4].text
+
+# end_date
+wait.until { driver.find_element(:class, "tbl-td-center") }
+end_date = driver.find_elements(:class, "tbl-td-center")[0].text
+
+# usage
+usage = driver.find_elements(:class, "tbl-td-center")[2].text
+
+# output
+puts ("Bill Amount: #{bill_amount}
+Bill Due Date: #{due_date}
+Usage: #{usage} kWh
+Start Date: #{start_date}
+End Date: #{end_date}")
